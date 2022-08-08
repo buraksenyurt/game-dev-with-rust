cbd75c8e94b26e3e6d3f8ab0719e05f1c2300759
buraksenyurt <burakselimsenyurt@gmail.com>
2022-08-08 22:20:42 +0300

    Added first window.

--- crayz-invaders/src/main.rs
+++ crayz-invaders/src/main.rs
@@ -1,3 +1,14 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
+        .insert_resource(WindowDescriptor {
+            title: "Crayz Invaders".to_string(),
+            width: 640.0,
+            height: 480.0,
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .run();
 }
