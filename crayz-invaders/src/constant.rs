72c2365dc8e36adfd86bc839e2ad03bac1da2d24
buraksenyurt <burakselimsenyurt@gmail.com>
2022-08-08 22:55:00 +0300

    Added the player sprite object to screen and created first system.

---
+++ crayz-invaders/src/constant.rs
@@ -0,0 +1,3 @@
+pub const PLAYER_SPRITE: &str = "player_01.png";
+pub const PLAYER_SIZE: (f32, f32) = (320., 320.);
+pub const SPRITE_SCALE: f32 = 0.15;
--- crayz-invaders/src/main.rs
+++ crayz-invaders/src/main.rs
@@ -1,3 +1,6 @@
+mod constant;
+
+use crate::constant::*;
 use bevy::prelude::*;
 
 fn main() {
@@ -5,10 +8,35 @@ fn main() {
         .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
         .insert_resource(WindowDescriptor {
             title: "Crayz Invaders".to_string(),
-            width: 640.0,
-            height: 480.0,
+            width: 480.0,
+            height: 640.0,
             ..Default::default()
         })
         .add_plugins(DefaultPlugins)
+        .add_startup_system(init_system)
         .run();
+}
+
+fn init_system(
+    mut commands: Commands,
+    mut windows: ResMut<Windows>,
+    asset_server: Res<AssetServer>,
+) {
+    commands.spawn_bundle(Camera2dBundle::default());
+
+    let main_window = windows.get_primary_mut().unwrap();
+    main_window.set_position(IVec2::new(0, 0));
+
+    let (_width, height) = (main_window.width(), main_window.height());
+    let bottom = -height / 2.;
+
+    commands.spawn_bundle(SpriteBundle {
+        texture: asset_server.load(PLAYER_SPRITE),
+        transform: Transform {
+            translation: Vec3::new(0., bottom + PLAYER_SIZE.1 / 2. * SPRITE_SCALE + 5., 10.),
+            scale: Vec3::new(SPRITE_SCALE, SPRITE_SCALE, 1.),
+            ..Default::default()
+        },
+        ..Default::default()
+    });
 }
