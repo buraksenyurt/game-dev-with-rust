47ee3473407ff27fb0b4153653471edcfe0f1d06
buraksenyurt <burakselimsenyurt@gmail.com>
2022-08-10 21:44:11 +0300

    Random x,y location creation added to enemy entity.

--- crayz-invaders/src/enemy.rs
+++ crayz-invaders/src/enemy.rs
@@ -1,5 +1,6 @@
-use crate::{GameTextures, SPRITE_SCALE};
+use crate::{GameTextures, WinSize, SPRITE_SCALE};
 use bevy::prelude::*;
+use rand::{thread_rng, Rng};
 
 pub struct EnemyPlugin;
 
@@ -9,10 +10,21 @@ impl Plugin for EnemyPlugin {
     }
 }
 
-fn create_system(mut commands: Commands, game_textures: Res<GameTextures>) {
+fn create_system(
+    mut commands: Commands,
+    game_textures: Res<GameTextures>,
+    window_size: Res<WinSize>,
+) {
+    let mut rnd = thread_rng();
+    let w = window_size.width / 2. - 100.;
+    let h = window_size.height / 2. - 150.;
+    let x = rnd.gen_range(-w..w);
+    let y = rnd.gen_range(-h..h);
+
     commands.spawn_bundle(SpriteBundle {
         texture: game_textures.enemy.clone(),
         transform: Transform {
+            translation: Vec3::new(x, y, 10.),
             scale: Vec3::new(SPRITE_SCALE, SPRITE_SCALE, 1.),
             ..Default::default()
         },
