6181e3deb012ef9c86acdf1d41627eebebcd3532
buraksenyurt <burakselimsenyurt@gmail.com>
2022-08-15 22:33:48 +0300

    Rackets redrawed by using DrawParam.

--- ping_pong/src/constants.rs
+++ ping_pong/src/constants.rs
@@ -1,4 +1,4 @@
-pub const RACKET_H: f32 = 100.;
-pub const RACKET_W: f32 = 20.;
+pub const RACKET_H: f32 = 75.;
+pub const RACKET_W: f32 = 15.;
 pub const RACKET_W_HALF: f32 = RACKET_W * 0.5;
 pub const RACKET_H_HALF: f32 = RACKET_H * 0.5;
--- ping_pong/src/main_state.rs
+++ ping_pong/src/main_state.rs
@@ -1,12 +1,29 @@
 use crate::constants::{RACKET_H, RACKET_H_HALF, RACKET_W, RACKET_W_HALF};
 use ggez::event::EventHandler;
+use ggez::graphics::DrawParam;
+use ggez::mint::Point2;
 use ggez::{graphics, Context, GameResult};
 
-pub struct MainState {}
+pub struct MainState {
+    p1_position: Point2<f32>,
+    p2_position: Point2<f32>,
+}
 
 impl MainState {
-    pub fn new(_ctx: &mut Context) -> Self {
-        MainState {}
+    pub fn new(ctx: &mut Context) -> Self {
+        let (scr_width, scr_height) = graphics::drawable_size(ctx);
+        //println!("{}X{}", scr_width, scr_height);
+        let (_scr_width_half, scr_height_half) = (scr_width * 0.5, scr_height * 0.5);
+        MainState {
+            p1_position: Point2 {
+                x: RACKET_W_HALF,
+                y: scr_height_half,
+            },
+            p2_position: Point2 {
+                x: scr_width - RACKET_W_HALF,
+                y: scr_height_half,
+            },
+        }
     }
 }
 
@@ -24,7 +41,9 @@ impl EventHandler for MainState {
             racket,
             graphics::Color::WHITE,
         )?;
-        graphics::draw(ctx, &racket_mesh, graphics::DrawParam::default())?;
+
+        graphics::draw(ctx, &racket_mesh, DrawParam::new().dest(self.p1_position))?;
+        graphics::draw(ctx, &racket_mesh, DrawParam::new().dest(self.p2_position))?;
 
         graphics::present(ctx)?;
         Ok(())
