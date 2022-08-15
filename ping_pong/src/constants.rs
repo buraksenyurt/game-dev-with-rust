1a455d46599f5390141fa32590cc23f931cca147
buraksenyurt <burakselimsenyurt@gmail.com>
2022-08-15 22:43:26 +0300

    Added ball object.

--- ping_pong/src/constants.rs
+++ ping_pong/src/constants.rs
@@ -2,3 +2,5 @@ pub const RACKET_H: f32 = 75.;
 pub const RACKET_W: f32 = 15.;
 pub const RACKET_W_HALF: f32 = RACKET_W * 0.5;
 pub const RACKET_H_HALF: f32 = RACKET_H * 0.5;
+pub const BALL_SIZE: f32 = 32.;
+pub const BALL_SIZE_HALF: f32 = BALL_SIZE * 0.5;
--- ping_pong/src/main_state.rs
+++ ping_pong/src/main_state.rs
@@ -1,4 +1,6 @@
-use crate::constants::{RACKET_H, RACKET_H_HALF, RACKET_W, RACKET_W_HALF};
+use crate::constants::{
+    BALL_SIZE, BALL_SIZE_HALF, RACKET_H, RACKET_H_HALF, RACKET_W, RACKET_W_HALF,
+};
 use ggez::event::EventHandler;
 use ggez::graphics::DrawParam;
 use ggez::mint::Point2;
@@ -7,13 +9,14 @@ use ggez::{graphics, Context, GameResult};
 pub struct MainState {
     p1_position: Point2<f32>,
     p2_position: Point2<f32>,
+    ball_position: Point2<f32>,
 }
 
 impl MainState {
     pub fn new(ctx: &mut Context) -> Self {
         let (scr_width, scr_height) = graphics::drawable_size(ctx);
         //println!("{}X{}", scr_width, scr_height);
-        let (_scr_width_half, scr_height_half) = (scr_width * 0.5, scr_height * 0.5);
+        let (scr_width_half, scr_height_half) = (scr_width * 0.5, scr_height * 0.5);
         MainState {
             p1_position: Point2 {
                 x: RACKET_W_HALF,
@@ -21,6 +24,10 @@ impl MainState {
             },
             p2_position: Point2 {
                 x: scr_width - RACKET_W_HALF,
+                y: scr_height_half,
+            },
+            ball_position: Point2 {
+                x: scr_width_half,
                 y: scr_height_half,
             },
         }
@@ -44,6 +51,16 @@ impl EventHandler for MainState {
 
         graphics::draw(ctx, &racket_mesh, DrawParam::new().dest(self.p1_position))?;
         graphics::draw(ctx, &racket_mesh, DrawParam::new().dest(self.p2_position))?;
+
+        let ball = graphics::Rect::new(-BALL_SIZE_HALF, -BALL_SIZE_HALF, BALL_SIZE, BALL_SIZE);
+        let ball_mesh = graphics::Mesh::new_rectangle(
+            ctx,
+            graphics::DrawMode::fill(),
+            ball,
+            graphics::Color::WHITE,
+        )?;
+
+        graphics::draw(ctx, &ball_mesh, DrawParam::new().dest(self.ball_position))?;
 
         graphics::present(ctx)?;
         Ok(())
