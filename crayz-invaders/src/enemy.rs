364e9e37ced5a784683eb9aa4d65cd3498134987
buraksenyurt <burakselimsenyurt@gmail.com>
2022-08-15 21:32:58 +0300

    Added fire system for enemies.

--- crayz-invaders/src/enemy.rs
+++ crayz-invaders/src/enemy.rs
@@ -1,8 +1,13 @@
-use crate::components::{Enemy, SpriteSize};
-use crate::{EnemyCount, GameTextures, WinSize, ENEMY_SIZE, MAX_ENEMY, SPRITE_SCALE};
+use crate::components::{Enemy, FromEnemy, SpriteSize};
+use crate::{
+    EnemyCount, GameTextures, Laser, Movable, Velocity, WinSize, ENEMY_LASER_SIZE, ENEMY_SIZE,
+    MAX_ENEMY, SPRITE_SCALE,
+};
+use bevy::ecs::schedule::ShouldRun;
 use bevy::prelude::*;
 use bevy::time::FixedTimestep;
 use rand::{thread_rng, Rng};
+use std::f32::consts::PI;
 
 pub struct EnemyPlugin;
 
@@ -12,7 +17,47 @@ impl Plugin for EnemyPlugin {
             SystemSet::new()
                 .with_run_criteria(FixedTimestep::step(0.75))
                 .with_system(enemy_create_system),
+        )
+        .add_system_set(
+            SystemSet::new()
+                .with_run_criteria(enemy_fire_criteria)
+                .with_system(enemy_fire_system),
         );
+    }
+}
+
+fn enemy_fire_system(
+    mut commands: Commands,
+    game_textures: Res<GameTextures>,
+    enemy_query: Query<&Transform, With<Enemy>>,
+) {
+    for &t in enemy_query.iter() {
+        let (x, y) = (t.translation.x, t.translation.y);
+
+        commands
+            .spawn_bundle(SpriteBundle {
+                texture: game_textures.enemy_laser.clone(),
+                transform: Transform {
+                    translation: Vec3::new(x, y - 15., 0.),
+                    rotation: Quat::from_rotation_x(PI),
+                    scale: Vec3::new(SPRITE_SCALE, SPRITE_SCALE, 1.),
+                    ..Default::default()
+                },
+                ..Default::default()
+            })
+            .insert(Laser)
+            .insert(SpriteSize::from(ENEMY_LASER_SIZE))
+            .insert(FromEnemy)
+            .insert(Movable { despawnable: true })
+            .insert(Velocity { x: 0., y: -1. });
+    }
+}
+
+fn enemy_fire_criteria() -> ShouldRun {
+    if thread_rng().gen_bool(1. / 60.) {
+        ShouldRun::Yes
+    } else {
+        ShouldRun::No
     }
 }
 
