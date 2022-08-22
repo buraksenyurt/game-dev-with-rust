ad1f389b3c8c92cd7440c4ed02a94ffb1f36e683
buraksenyurt <burakselimsenyurt@gmail.com>
2022-08-22 22:10:26 +0300

    Drawing a simple Vertical Rectangle.

--- ggez_101/src/game.rs
+++ ggez_101/src/game.rs
@@ -1,5 +1,7 @@
 use ggez::event::EventHandler;
-use ggez::{graphics, Context, GameError};
+use ggez::graphics::{draw, DrawParam};
+use ggez::mint::Point2;
+use ggez::{graphics, Context, GameResult};
 
 pub struct Game {}
 
@@ -10,12 +12,32 @@ impl Game {
 }
 
 impl EventHandler for Game {
-    fn update(&mut self, _ctx: &mut Context) -> Result<(), GameError> {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
         Ok(())
     }
 
-    fn draw(&mut self, ctx: &mut Context) -> Result<(), GameError> {
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        let screen = graphics::drawable_size(&ctx);
+
         graphics::clear(ctx, graphics::Color::from_rgb(17, 65, 143));
+
+        // draw vertical rectangle
+        let column = graphics::Rect::new(0., 0., 25., 200.);
+        let column_mesh = graphics::Mesh::new_rectangle(
+            ctx,
+            graphics::DrawMode::fill(),
+            column,
+            graphics::Color::WHITE,
+        )?;
+
+        draw(
+            ctx,
+            &column_mesh,
+            DrawParam::new().dest(Point2 {
+                x: screen.0 / 2. - 12.5,
+                y: 5.,
+            }),
+        )?;
 
         graphics::present(ctx)?;
         Ok(())
