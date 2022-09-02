3003e475ae4c519dd6db14ebc2073dc7e7344fce
buraksenyurt <burakselimsenyurt@gmail.com>
2022-09-02 12:09:09 +0300

    Some required constants added.

---
+++ rockstroid/src/constant.rs
@@ -0,0 +1,12 @@
+pub const PLAYER_LIFE:f32=1.;
+pub const PLAYER_BOX_SIZE:f32=12.;
+
+pub const SHOT_LIFE:f32=2.;
+pub const SHOT_BOX_SIZE:f32=6.;
+pub const SHOT_SPEED:f32=200.;
+pub const SHOT_ANGLE_VELOCITY:f32=0.1;
+
+pub const ROCK_LIFE:f32=1.;
+pub const ROCK_BOX_SIZE:f32=12.;
+
+pub const MAX_ROCK_VELOCITY:f32=50.;
--- rockstroid/src/main.rs
+++ rockstroid/src/main.rs
@@ -1,3 +1,5 @@
+mod constant;
+
 fn main() {
     println!("Hello, world!");
 }
