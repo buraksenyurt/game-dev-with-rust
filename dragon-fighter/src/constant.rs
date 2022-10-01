dfd7753f0dc64d0bc11e555355b71dc4823b3542
buraksenyurt <burakselimsenyurt@gmail.com>
2022-10-01 22:38:32 +0300

    Player movement system integrated.

--- dragon-fighter/src/constant.rs
+++ dragon-fighter/src/constant.rs
@@ -2,3 +2,4 @@ use bevy::prelude::Color;
 
 pub const CLEAR_COLOR: Color = Color::rgb(0.1, 0.1, 0.1);
 pub const RESOLUTION: f32 = 16.0 / 9.0;
+pub const TILE_SIZE: f32 = 0.1;
--- dragon-fighter/src/system/player.rs
+++ dragon-fighter/src/system/player.rs
@@ -1,31 +1,70 @@
+use crate::constant::TILE_SIZE;
 use crate::system::texture::AsciiSheet;
 use bevy::prelude::*;
 
+// Plugin
 pub struct PlayerPlugin;
 
+// Oyuncu nesnesi için component tanımı
 #[derive(Component)]
-pub struct Player;
+pub struct Player {
+    pub speed: f32,
+}
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(spawn_player);
+        // Oyuncu için tasarlanan bu plugin'e oyuncu ile alakalı sistemleri aşağıdaki gibi
+        // ekleyebiliriz. Oyuncuyu oluşturan sistem, hareket sistemi vb
+        // Böylece main fonksiyonunda birçok sistem tanımlamak yerine oyuncu plugin nesnesini
+        // kullanabiliriz.
+        app.add_startup_system(spawn_player)
+            .add_system(player_movement);
     }
 }
 
-pub fn spawn_player(mut commands: Commands, ascii: Res<AsciiSheet>) {
+// Oyuncu hareket sistemi
+// Generic olan ilk parametre Player component'i ve bir Transform nesnesi kullanır.
+// Transform nesnesi ile oyuncunun o anki pozisyon bilgisini alabilir ve değiştirebiliriz.
+// Böylece bu sistemin player bileşenini kullanması sağlanır.
+// Sisteme giren ikinci parametre ise klavye tuşlarını ele almak içindir.
+// Son olarak dahil edilen 3ncü parametre delta_time bilgisini kullanmak için bir kaynaktır.
+fn player_movement(
+    mut player_query: Query<(&Player, &mut Transform)>,
+    keyboard: Res<Input<KeyCode>>,
+    time: Res<Time>,
+) {
+    // sahada tek bir oyuncu olduğundan single_mut kullandık ve
+    let (player, mut transform) = player_query.single_mut();
+
+    // Basılan yön tuşlarına göre transform nesnesi üstünden oyuncuya bir hareket veriyoruz
+    if keyboard.pressed(KeyCode::Up) {
+        transform.translation.y += player.speed * TILE_SIZE * time.delta_seconds();
+    }
+    if keyboard.pressed(KeyCode::Down) {
+        transform.translation.y -= player.speed * TILE_SIZE * time.delta_seconds();
+    }
+    if keyboard.pressed(KeyCode::Left) {
+        transform.translation.x -= player.speed * TILE_SIZE * time.delta_seconds();
+    }
+    if keyboard.pressed(KeyCode::Right) {
+        transform.translation.x += player.speed * TILE_SIZE * time.delta_seconds();
+    }
+}
+
+fn spawn_player(mut commands: Commands, ascii: Res<AsciiSheet>) {
     let mut player_texture = TextureAtlasSprite::new(14);
     player_texture.color = Color::rgb(255., 215., 0.);
-    player_texture.custom_size = Some(Vec2::splat(0.1));
+    player_texture.custom_size = Some(Vec2::splat(TILE_SIZE));
     commands
         .spawn_bundle(SpriteSheetBundle {
             sprite: player_texture,
             texture_atlas: ascii.0.clone(),
             transform: Transform {
-                translation: Vec3::new(0., 0., 0.),
+                translation: Vec3::new(2. * TILE_SIZE, -2. * TILE_SIZE, 900.),
                 ..Default::default()
             },
             ..Default::default()
         })
         .insert(Name::new("AgentSmith"))
-        .insert(Player);
+        .insert(Player { speed: 5. });
 }
