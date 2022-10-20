cafcf27c527ae924ce930cd584085a9176d96532
buraksenyurt <burakselimsenyurt@gmail.com>
2022-10-20 17:08:52 +0300

    Added paddle2 texture to game state.

--- tetra-pong/src/game_state.rs
+++ tetra-pong/src/game_state.rs
@@ -1,8 +1,8 @@
 /*
 Oyun durum bilgilerini tutan State nesnesi ve implementasyonu
 */
-use crate::constant::{OCEAN_BLUE, PADDLE1_PATH};
-use crate::SCREEN_WIDTH;
+use crate::constant::{OCEAN_BLUE, PADDLE1_PATH, PADDLE2_PATH};
+use crate::{SCREEN_HEIGHT, SCREEN_WIDTH};
 use tetra::graphics::{Color, Texture};
 use tetra::math::Vec2;
 use tetra::{graphics, Context, State, TetraError};
@@ -10,6 +10,8 @@ use tetra::{graphics, Context, State, TetraError};
 pub struct GameState {
     pub paddle1_texture: Texture,
     pub paddle1_position: Vec2<f32>,
+    pub paddle2_texture: Texture,
+    pub paddle2_position: Vec2<f32>,
 }
 
 impl GameState {
@@ -17,11 +19,21 @@ impl GameState {
     pub fn new(context: &mut Context) -> tetra::Result<GameState> {
         // Paddle1 imajını alıyoruz
         let paddle1_texture = Texture::new(context, PADDLE1_PATH)?;
-        // başlangıç pozisyonunu ayarlıyoruz. Ekranın üstünden ortada.
+        // Paddle1 için başlangıç pozisyonunu ayarlıyoruz. Ekranın üstünden ortada.
         let paddle1_position = Vec2::new((SCREEN_WIDTH - paddle1_texture.width() as f32) * 0.5, 0.);
+
+        // Paddle2 içinde resim yüklemesi ve başlangıç konumlarını ayarlıyoruz.
+        let paddle2_texture = Texture::new(context, PADDLE2_PATH)?;
+        let paddle2_position = Vec2::new(
+            (SCREEN_WIDTH - paddle2_texture.width() as f32) * 0.5,
+            SCREEN_HEIGHT - paddle2_texture.height() as f32,
+        );
+
         Ok(GameState {
             paddle1_texture,
             paddle1_position,
+            paddle2_texture,
+            paddle2_position,
         })
     }
 }
@@ -33,6 +45,8 @@ impl State for GameState {
         graphics::clear(context, Color::hex(OCEAN_BLUE));
         // İlk oyuncunun raketini ekrana çizdiriyoruz
         self.paddle1_texture.draw(context, self.paddle1_position);
+        // İkinci oyuncunun raketi çizdirilir
+        self.paddle2_texture.draw(context, self.paddle2_position);
         Ok(())
     }
 }
