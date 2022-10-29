51829b7bee676e04a2bfa793d6c24df7a3a67670
buraksenyurt <burakselimsenyurt@gmail.com>
2022-10-29 23:01:43 +0300

    Increased the bullet flight time.

--- wolf-tank/src/constant.rs
+++ wolf-tank/src/constant.rs
@@ -1,2 +1,6 @@
 pub const TANK_ROTATION_VALUE: f32 = 1.;
 pub const MENU_SEPERATOR_HEIGHT: f32 = 25.;
+pub const BULLET_SPEED: f32 = 2.5;
+pub const MAX_SHOOT_AT_TIME: f64 = 6.;
+pub const BULLET_RELOAD_TIME: f64 = 0.5;
+pub const MAX_BULLET_COUNT_IN_RANGE: usize = 5;
--- wolf-tank/src/main.rs
+++ wolf-tank/src/main.rs
@@ -7,7 +7,10 @@ mod resource;
 mod tank;
 
 use crate::bullet::Bullet;
-use crate::constant::TANK_ROTATION_VALUE;
+use crate::constant::{
+    BULLET_RELOAD_TIME, BULLET_SPEED, MAX_BULLET_COUNT_IN_RANGE, MAX_SHOOT_AT_TIME,
+    TANK_ROTATION_VALUE,
+};
 use crate::game_state::GameState;
 use crate::helper::border_check;
 use crate::menu::draw_menu;
@@ -46,17 +49,20 @@ async fn main() {
                 let position = border_check(&player_tank.position, player_tank.texture.width());
                 player_tank.position = position;
                 if is_key_down(KeyCode::Up) {
-                    //println!("{}", player_tank);
                     player_tank.position += direction;
                 } else if is_key_down(KeyCode::Down) {
                     player_tank.position -= direction;
                 }
 
-                // 0.5 değeri ile oynayarak bir atıştan ne kadar sonra ateş edebileceğimizi belirtebiliriz.
-                if is_key_down(KeyCode::S) && delta_time - last_shot > 0.5 {
+                // BULLET_RELOAD_TIME değeri ile oynayarak
+                // bir atıştan ne kadar sonra ateş edebileceğimizi belirtebiliriz.
+                if is_key_down(KeyCode::S)
+                    && delta_time - last_shot > BULLET_RELOAD_TIME
+                    && bullets.len() < MAX_BULLET_COUNT_IN_RANGE
+                {
                     let bullet = Bullet {
                         position: player_tank.position + rotation,
-                        velocity: direction * 5. * delta_time as f32,
+                        velocity: direction * delta_time as f32,
                         shoot_at: get_time(),
                         collided: false,
                         texture: bullet_texture,
@@ -72,7 +78,7 @@ async fn main() {
                     player_tank.rotation -= TANK_ROTATION_VALUE * get_frame_time();
                 }
 
-                bullets.retain(|bullet| bullet.shoot_at + 6. > delta_time);
+                bullets.retain(|bullet| bullet.shoot_at + MAX_SHOOT_AT_TIME > delta_time);
                 println!("Total bullets in battlefield {}", bullets.len());
             }
             GameState::PlayerDead => {}
@@ -88,7 +94,7 @@ async fn main() {
                 for b in bullets.iter_mut() {
                     let rotation = b.rotation;
                     let direction = Vec2::new(rotation.cos(), rotation.sin());
-                    b.position += direction;
+                    b.position += direction * BULLET_SPEED;
                     b.draw();
                 }
             }
