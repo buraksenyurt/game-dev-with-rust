e11704da653a1d55ce17494bcb5c790627d07586
buraksenyurt <burakselimsenyurt@gmail.com>
2022-11-05 22:12:48 +0300

    Trying to calculate right line equation.

--- fire-and-forget/src/main.rs
+++ fire-and-forget/src/main.rs
@@ -11,15 +11,15 @@ async fn main() {
     let ferris_texture = load_texture("assets/ferris.png").await.unwrap();
     let ref_point = vec2(
         screen_width() * 0.5 - ferris_texture.width() * 0.5,
-        screen_height() - ferris_texture.height(),
+        screen_height() - (ferris_texture.height() + 10.),
     );
     let mut ferris = Rusty {
         start_position: ref_point,
         current_position: ref_point,
-        speed: Default::default(),
-        direction: Default::default(),
         color: Default::default(),
     };
+    let mut clickable = true;
+
     clear_background(Color::default());
     let mut grade = 0.;
     let mut n: f32 = 0.;
@@ -31,32 +31,34 @@ async fn main() {
                 rand::gen_range(100, 240),
                 255,
             );
-            let m_pos = vec2(mouse_position().0, mouse_position().1);
-            // Mouse'a tıklanan yer ile ateşe başlangıç noktası arasındaki doğru denkleminin bulunması
-            // Eğimi bul
-            grade = (ref_point.y - m_pos.y).abs() / (ref_point.x - m_pos.x).abs();
-            if m_pos.y < ref_point.y {
+            if clickable {
+                let m_pos = vec2(mouse_position().0, mouse_position().1);
+                // Mouse'a tıklanan yer ile ateşe başlangıç noktası arasındaki doğru denkleminin bulunması
+                // Eğimi bul
+                grade = (m_pos.y - ref_point.y).abs() / (m_pos.x - ref_point.x).abs();
                 if m_pos.x > ref_point.x {
                     grade = -grade;
-                } else {
+                } else if m_pos.x < ref_point.x {
                     grade = grade;
+                } else {
+                    grade = 0.;
                 }
-            } else if m_pos.y == ref_point.y {
-                grade = 0.;
+                n = ref_point.y - (grade * ref_point.x);
+                println!(
+                    "Grade is {}\nFerris {}\nMouse {}",
+                    grade, ferris.current_position, m_pos
+                );
+                clickable = false;
             }
-            n = ref_point.y - (grade * ref_point.x);
-            println!(
-                "Grade is {}\nFerris {}\nMouse {}",
-                grade, ferris.current_position, m_pos
-            );
             // y -  referans noktanın y değeri = grade x (x - referans noktanın x değeri)
         }
+
         if grade < 0. {
             ferris.current_position.x += 1.;
         } else {
             ferris.current_position.x -= 1.;
         }
-        ferris.current_position.y = grade * ferris.current_position.x + n;
+        ferris.current_position.y = (grade * ferris.current_position.x) + n;
 
         if ferris.current_position.x < 0.
             || ferris.current_position.x > screen_width()
@@ -65,10 +67,9 @@ async fn main() {
             ferris = Rusty {
                 start_position: ref_point,
                 current_position: ref_point,
-                speed: Default::default(),
-                direction: Default::default(),
                 color: Default::default(),
             };
+            clickable = true;
         }
         // println!("Current Position x,y {}", ferris.current_position);
         // println!("Start Position x,y {}", ferris.current_position);
--- fire-and-forget/src/rusty.rs
+++ fire-and-forget/src/rusty.rs
@@ -3,7 +3,5 @@ use macroquad::prelude::{Color, Vec2};
 pub struct Rusty {
     pub start_position: Vec2,
     pub current_position: Vec2,
-    pub direction: Vec2,
-    pub speed: Vec2,
     pub color: Color,
 }
