5bdbfe8cabba0279f561ad6685a1695293dc1b81
buraksenyurt <burakselimsenyurt@gmail.com>
2022-11-11 02:16:01 +0300

    I am trying to find neighbors of current cell.

--- life-game/src/main.rs
+++ life-game/src/main.rs
@@ -1,17 +1,17 @@
 use macroquad::prelude::*;
 use std::fmt::{Display, Formatter};
 
-const WINDOW_WIDTH: i32 = 320;
-const WINDOW_HEIGHT: i32 = 320;
-const CELL_SIZE: i32 = 32;
+const WINDOW_WIDTH: f32 = 320.;
+const WINDOW_HEIGHT: f32 = 320.;
+const CELL_SIZE: f32 = 32.;
 
 fn window_conf() -> Conf {
     Conf {
         window_title: "Conway's Game of Life".to_string(),
         fullscreen: false,
         window_resizable: false,
-        window_width: WINDOW_WIDTH,
-        window_height: WINDOW_HEIGHT,
+        window_width: WINDOW_WIDTH as i32,
+        window_height: WINDOW_HEIGHT as i32,
         ..Default::default()
     }
 }
@@ -19,77 +19,69 @@ fn window_conf() -> Conf {
 #[macroquad::main(window_conf)]
 async fn main() {
     let mut cells = Vec::new();
-    let (row, col) = (WINDOW_HEIGHT / CELL_SIZE, WINDOW_WIDTH / CELL_SIZE);
-    let mut index = 0;
-    for i in 0..row {
-        for j in 0..col {
-            index += 1;
-            let state = match rand::gen_range(0, 10) {
+    let (row, col) = (
+        (WINDOW_HEIGHT / CELL_SIZE) as i32,
+        (WINDOW_WIDTH / CELL_SIZE) as i32,
+    );
+    println!("{} X {}", row, col);
+    let mut counter: usize = 0;
+    for i in 0..col {
+        // println!("{}. satır", i);
+        for j in 0..row {
+            // println!("\t{} sütun", j);
+            let x = j as f32 * CELL_SIZE;
+            let y = i as f32 * CELL_SIZE;
+
+            let state = match rand::gen_range(0, 5) {
                 0 => CellState::Alive,
                 _ => CellState::Dead,
             };
-            let c = Cell::new(
-                index,
-                Vec2::new((i * CELL_SIZE) as f32, (j * CELL_SIZE) as f32),
-                state,
-            );
-            let _ = &cells.push(c);
+            let c = Cell::new(counter, Vec2::new(x, y), state);
+            cells.push(c);
+            counter += 1;
             //print!("{},", c);
         }
     }
+
     loop {
         clear_background(WHITE);
 
-        for i in 0..99 {
-            let c = &cells[i];
-            let mut current_id = c.id;
+        for i in 0..cells.len() {
+            let mut c = cells[i];
             let mut live_neighbors_count = 0;
-            let mut dead_neighbors_count = 0;
-            println!("{}", current_id);
 
-            if current_id % col == 0 || current_id % col < (col - 1) {
-                match cells[(current_id + 1) as usize].state {
-                    CellState::Alive => live_neighbors_count += 1,
-                    CellState::Dead => dead_neighbors_count += 1,
-                }
+            // Sağ komşu kontrolü
+            if Vec2::new(c.position.x + 32., c.position.y).x < WINDOW_WIDTH {
+                if let CellState::Alive = cells[c.id + 1].state {
+                    live_neighbors_count += 1;
+                };
             }
-            if (current_id - 1) % col == 0 || (current_id - 1) % col < (col - 1) {
-                match cells[(current_id - 1) as usize].state {
-                    CellState::Alive => live_neighbors_count += 1,
-                    CellState::Dead => dead_neighbors_count += 1,
-                }
-            }
-
-            match cells.get((current_id - col) as usize) {
-                Some(_) => match cells[(current_id - col) as usize].state {
-                    CellState::Alive => live_neighbors_count += 1,
-                    CellState::Dead => dead_neighbors_count += 1,
-                },
-                _ => {}
-            };
-            match cells.get((current_id + col) as usize) {
-                Some(_) => match cells[(current_id + col) as usize].state {
-                    CellState::Alive => live_neighbors_count += 1,
-                    CellState::Dead => dead_neighbors_count += 1,
-                },
-                _ => {}
+            // Sol komşu kontrolü
+            if Vec2::new(c.position.x - 32., c.position.y).x > 0. {
+                if let CellState::Alive = cells[c.id - 1].state {
+                    live_neighbors_count += 1;
+                };
             }
 
             match c.state {
                 CellState::Alive => {
-                    if (live_neighbors_count == 0) {
-                        println!("Hücre canlı ama hiç komşusu yok");
-                    } else if (live_neighbors_count < 2) {
-                        println!("Hücre canlı ve 2 den az canlı komşusu var");
-                    } else if (live_neighbors_count == 2 || live_neighbors_count == 3) {
-                        println!("Hücre canlı ve 2 veya 3 canlı komşusu var");
-                    } else if (live_neighbors_count > 3) {
-                        println!("Hücre canlı ve 3 ten fazla komşusu var");
+                    if live_neighbors_count < 2 {
+                        // println!("{} canlı komşu sayısı 2den az", current_id);
+                        c.state = CellState::Dead;
+                    } else if live_neighbors_count == 2 || live_neighbors_count == 3 {
+                        // println!("{} canlı komşu sayısı 2 veya 3", current_id);
+                        c.state = CellState::Alive
+                    } else if live_neighbors_count > 3 {
+                        // println!("{} canlı komşu sayısı 3den fazla", current_id);
+                        c.state = CellState::Dead;
+                    } else {
+                        // println!("{} hiç komşu yok gibi", current_id);
                     }
                 }
                 CellState::Dead => {
-                    if (live_neighbors_count == 3) {
-                        println!("Hücre ölü ama 3 canlı komşusu var");
+                    if live_neighbors_count == 3 {
+                        // println!("{} ölü ama 3 canlı komşusu var", current_id);
+                        c.state = CellState::Alive
                     }
                 }
             }
@@ -98,11 +90,12 @@ async fn main() {
         for c in &cells {
             c.draw();
         }
+
         next_frame().await
     }
 }
 
-#[derive(Copy, Clone, PartialEq)]
+#[derive(Copy, Clone)]
 pub enum CellState {
     Alive,
     Dead,
@@ -119,13 +112,13 @@ impl Display for CellState {
 
 #[derive(Clone, Copy)]
 pub struct Cell {
-    id: i32,
+    id: usize,
     state: CellState,
     position: Vec2,
 }
 
 impl Cell {
-    pub fn new(id: i32, position: Vec2, state: CellState) -> Self {
+    pub fn new(id: usize, position: Vec2, state: CellState) -> Self {
         Self {
             id,
             state,
@@ -133,7 +126,7 @@ impl Cell {
         }
     }
     pub fn draw(&self) {
-        let color = match self.state {
+        let color = match &self.state {
             CellState::Alive => BLACK,
             CellState::Dead => WHITE,
         };
@@ -149,6 +142,6 @@ impl Cell {
 
 impl Display for Cell {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}-> {}: {}", self.id, self.position, self.state)
+        write!(f, "[{}] {}: {}", self.id, self.position, self.state)
     }
 }
