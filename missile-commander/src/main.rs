c6f0993636d8fc1fa1820873bfab949460b638c3
buraksenyurt <burakselimsenyurt@gmail.com>
2022-11-12 17:59:03 +0300

    Random angle generator for missile.

--- missile-commander/src/main.rs
+++ missile-commander/src/main.rs
@@ -1,20 +1,17 @@
 use macroquad::prelude::{
     clear_background, draw_rectangle, next_frame, screen_width, Color, Vec2, WHITE,
 };
+use macroquad::window::screen_height;
 use rand::prelude::*;
 
 #[macroquad::main("Missile Command")]
 async fn main() {
-    let mut rng = thread_rng();
-    let x = rng.gen_range((0. + screen_width() * 0.25)..(screen_width() - screen_width() * 0.25));
-
-    let mut missile = Missile {
-        position: Vec2::new(x, 0.),
-    };
+    let rng = thread_rng();
+    let mut missile = Missile::produce(rng);
 
     loop {
         clear_background(Color::default());
-        missile.position += Vec2::new(0., 1.);
+        missile.position += missile.direction;
 
         draw_rectangle(missile.position.x, missile.position.y, 4., 4., WHITE);
 
@@ -24,4 +21,32 @@ async fn main() {
 
 struct Missile {
     pub position: Vec2,
+    pub direction: Vec2,
+}
+
+impl Missile {
+    pub fn produce(mut rng: ThreadRng) -> Self {
+        let x =
+            rng.gen_range((0. + screen_width() * 0.25)..(screen_width() - screen_width() * 0.25));
+        let left_angle = (x / screen_height()).atan();
+        let right_angle = ((screen_width() - x) / screen_height()).atan();
+        println!("x value {}", x);
+        println!(
+            "Left angle(max) - {} Radian \t Right angle(max) - {} Radian\nLeft angle(max) - {} Degree \t Right angle(max) - {} Degree",
+            left_angle, right_angle,left_angle.to_degrees(),
+            right_angle.to_degrees()
+        );
+        let pos_neg = rng.gen::<bool>();
+        let v = match pos_neg {
+            true => 1.,
+            false => -1.,
+        };
+        let angle: f32 = rng.gen_range(left_angle..right_angle) * v;
+        println!("Angle {}", angle);
+
+        Self {
+            position: Vec2::new(x, 0.),
+            direction: Vec2::new(angle.sin(), 1.),
+        }
+    }
 }
