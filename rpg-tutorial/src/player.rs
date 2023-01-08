429244527235f4b1d1579b8b718776ca453e9e89
buraksenyurt <burakselimsenyurt@gmail.com>
2023-01-08 22:18:19 +0300

    Added tile collider which use AABB.

--- rpg-tutorial/src/player.rs
+++ rpg-tutorial/src/player.rs
@@ -1,6 +1,8 @@
 use crate::ascii::{spawn_sprite, AsciiSheet};
+use crate::tile_map::TileCollider;
 use crate::TILE_SIZE;
 use bevy::prelude::*;
+use bevy::sprite::collide_aabb::collide;
 
 #[derive(Component)]
 pub struct Player {
@@ -17,23 +19,51 @@ impl Plugin for PlayerPlugin {
 
 fn movement(
     mut query: Query<(&Player, &mut Transform)>,
+    wall_query: Query<&Transform, (With<TileCollider>, Without<Player>)>,
     keyboard: Res<Input<KeyCode>>,
     time: Res<Time>,
 ) {
     let (player, mut transform) = query.single_mut();
-
+    let (mut y_delta, mut x_delta) = (0., 0.);
     if keyboard.pressed(KeyCode::Up) {
-        transform.translation.y += TILE_SIZE * player.movement_speed * time.delta_seconds();
+        y_delta += TILE_SIZE * player.movement_speed * time.delta_seconds();
     }
     if keyboard.pressed(KeyCode::Down) {
-        transform.translation.y -= TILE_SIZE * player.movement_speed * time.delta_seconds();
+        y_delta -= TILE_SIZE * player.movement_speed * time.delta_seconds();
     }
     if keyboard.pressed(KeyCode::Left) {
-        transform.translation.x -= TILE_SIZE * player.movement_speed * time.delta_seconds();
+        x_delta -= TILE_SIZE * player.movement_speed * time.delta_seconds();
     }
     if keyboard.pressed(KeyCode::Right) {
-        transform.translation.x += TILE_SIZE * player.movement_speed * time.delta_seconds();
+        x_delta += TILE_SIZE * player.movement_speed * time.delta_seconds();
     }
+
+    let target = transform.translation + Vec3::new(x_delta, 0., 0.);
+    if is_collision_with_wall(target, &wall_query) {
+        transform.translation = target;
+    }
+    let target = transform.translation + Vec3::new(0., y_delta, 0.);
+    if is_collision_with_wall(target, &wall_query) {
+        transform.translation = target;
+    }
+}
+
+fn is_collision_with_wall(
+    player_pos: Vec3,
+    query: &Query<&Transform, (With<TileCollider>, Without<Player>)>,
+) -> bool {
+    for transform in query.iter() {
+        let collision = collide(
+            player_pos,
+            Vec2::splat(TILE_SIZE * 1.),
+            transform.translation,
+            Vec2::splat(TILE_SIZE * 0.9),
+        );
+        if collision.is_some() {
+            return false;
+        }
+    }
+    true
 }
 
 fn spawn(mut commands: Commands, ascii_res: Res<AsciiSheet>) {
--- rpg-tutorial/src/tile_map.rs
+++ rpg-tutorial/src/tile_map.rs
@@ -6,6 +6,9 @@ use std::io::{BufRead, BufReader};
 
 pub struct TileMapPlugin;
 
+#[derive(Component)]
+pub struct TileCollider;
+
 impl Plugin for TileMapPlugin {
     fn build(&self, app: &mut App) {
         app.add_startup_system(create);
@@ -13,18 +16,36 @@ impl Plugin for TileMapPlugin {
 }
 
 fn create(mut commands: Commands, ascii_res: Res<AsciiSheet>) {
+    let mut tiles = Vec::new();
     let map_file = File::open("assets/schene_01.txt").expect("Map file not found");
     for (y, line) in BufReader::new(map_file).lines().enumerate() {
         if let Ok(line) = line {
             for (x, char) in line.chars().enumerate() {
-                spawn_sprite(
+                let tile = spawn_sprite(
                     &mut commands,
                     &ascii_res,
                     char as usize,
                     Color::GOLD,
                     Vec3::new(x as f32 * TILE_SIZE, -(y as f32) * TILE_SIZE, 100.),
                 );
+                // Eğer haritadaki # (duvar) yükleniyorsa o entity için
+                // çarpışma bileşenini yükle.
+                // Böylece player'ın movement(hareket) sisteminde,
+                // TileCollider yüklü nesneleri sorgulayabilir ve duvarlardan geçilmesini
+                // engelleyebiliriz.
+                if char == '#' {
+                    commands.entity(tile).insert(TileCollider);
+                }
+                tiles.push(tile);
             }
         }
     }
+
+    commands
+        .spawn(Name::new("Map"))
+        .insert(ComputedVisibility::default())
+        .insert(Visibility::VISIBLE)
+        .insert(Transform::default())
+        .insert(GlobalTransform::default())
+        .push_children(&tiles);
 }
