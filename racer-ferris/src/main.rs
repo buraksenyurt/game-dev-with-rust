27693d7acf46b26889648494597474d9eee17ed1
buraksenyurt <burakselimsenyurt@gmail.com>
2023-01-15 20:21:09 +0300

    Added game state data structure.

--- racer-ferris/src/main.rs
+++ racer-ferris/src/main.rs
@@ -3,5 +3,26 @@ use rusty_engine::prelude::*;
 fn main() {
     let mut game = Game::new();
 
-    game.run(());
+    game.run(GameState::default());
+}
+
+struct Score {
+    high: u32,
+    current: u32,
+}
+
+struct GameState {
+    score: Score,
+    enemy_labels: Vec<String>,
+    spawn_timer: Timer,
+}
+
+impl Default for GameState{
+    fn default() -> Self {
+        Self{
+            score:Score{high:0,current:0},
+            enemy_labels:Vec::new(),
+            spawn_timer:Timer::from_seconds(1.,false)
+        }
+    }
 }
