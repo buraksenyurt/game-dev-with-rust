d202044391d0afdcaa68047b8837208baecd0305
buraksenyurt <burakselimsenyurt@gmail.com>
2023-01-31 16:31:57 +0300

    Added the obstacles into the game.

--- pod-race/src/main.rs
+++ pod-race/src/main.rs
@@ -1,3 +1,4 @@
+use rand::prelude::*;
 use rusty_engine::prelude::*;
 
 const PLAYER_SPEED: f32 = 500.;
@@ -20,19 +21,43 @@ impl Default for GameState {
 fn main() {
     let mut game = Game::new();
 
+    create_player(&mut game);
+    game.audio_manager.play_music("jamsi4.ogg", 0.2);
+    create_road_lines(&mut game);
+    create_obstacles(&mut game);
+
+    game.add_logic(game_logic);
+    game.run(GameState::default());
+}
+
+fn create_player(game: &mut Game<GameState>) {
     let player1 = game.add_sprite("player1", "sprite/player.png");
     player1.translation.x = -500.;
     player1.layer = 10.;
     player1.collision = true;
-    game.audio_manager.play_music("jamsi4.ogg", 0.2);
+}
+
+fn create_road_lines(game: &mut Game<GameState>) {
     for i in 0..10 {
         let line = game.add_sprite(format!("line_{}", i), "sprite/barrier_white.png");
         line.scale = 0.1;
         line.translation.x = -600. + 150. * i as f32;
     }
+}
 
-    game.add_logic(game_logic);
-    game.run(GameState::default());
+fn create_obstacles(game: &mut Game<GameState>) {
+    let presets = vec![
+        "sprite/barrel_red.png",
+        "sprite/barrel_blue.png",
+        "sprite/cone_straight.png",
+    ];
+    for (i, preset) in presets.into_iter().enumerate() {
+        let obstacle = game.add_sprite(format!("obstacle_{}", i), preset);
+        obstacle.layer = 5.;
+        obstacle.collision = true;
+        obstacle.translation.x = thread_rng().gen_range(800.0..1600.);
+        obstacle.translation.y = thread_rng().gen_range(-300.0..300.);
+    }
 }
 
 fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
@@ -56,11 +81,22 @@ fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
         game_state.health = 0;
     }
 
+    move_road_objects(engine);
+}
+
+fn move_road_objects(engine: &mut Engine) {
     for sprite in engine.sprites.values_mut() {
         if sprite.label.starts_with("line") {
             sprite.translation.x -= ROAD_SPEED * engine.delta_f32;
             if sprite.translation.x < -675. {
                 sprite.translation.x += 1500.
+            }
+        }
+        if sprite.label.starts_with("obstacle") {
+            sprite.translation.x -= ROAD_SPEED * engine.delta_f32;
+            if sprite.translation.x < -800. {
+                sprite.translation.x = thread_rng().gen_range(800.0..1600.);
+                sprite.translation.y = thread_rng().gen_range(-300.0..300.);
             }
         }
     }
