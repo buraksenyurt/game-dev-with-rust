984c798b6c1beec7ee33e067362ab0a6a588d209
buraksenyurt <burakselimsenyurt@gmail.com>
2023-03-11 22:05:03 +0300

    Applied the movement func by based keypress for pacman.

--- pacman/src/entities/pacman.rs
+++ pacman/src/entities/pacman.rs
@@ -1,7 +1,9 @@
+use crate::common::contants::TILE_MAP_WIDTH;
 use crate::common::direction::Direction;
 use crate::common::position::Position;
-use crate::entities::cell::Tile;
+use crate::entities::cell::{Tile, TileType};
 use crate::entities::map::Map;
+use piston::Key::P;
 
 pub struct Pacman {
     map: Map,
@@ -69,12 +71,62 @@ impl Pacman {
         if self.lives == 0 {
             return;
         }
-        //TODO Pacman hareket ettirilecek
+        self.go();
         //TODO Hayaletler hareket ettirilecek
         //TODO Hayalet ile pacman arasındaki etkileşimler(çarpışma gibi) kontrol edilecek
     }
 
     pub fn get_location(&self) -> (Position, Direction) {
         (Position::new(self.pos.x, self.pos.y), self.target_dir)
+    }
+
+    // Pacman karakterini hareket ettiren fonksiyon
+    fn go(&mut self) {
+        // Hedeflenen yöne doğru gidebilir mi bakılır
+        if self.can_turn() {
+            self.dir = self.target_dir;
+        }
+        // Basılan tuşun işaret ettiği yöne göre yeni pozisyon bulunur
+        let position = match self.dir {
+            Direction::Up => Position::new(self.pos.x, self.pos.y - 1),
+            Direction::Down => Position::new(self.pos.x, self.pos.y + 1),
+            Direction::Left => Position::new(self.pos.x - 1, self.pos.y),
+            Direction::Right => Position::new(self.pos.x + 1, self.pos.y),
+        };
+        // Elde edilen pozisyona göre haritada ne olduğuna bakılır
+        match self.map.get(&position) {
+            /*
+                map.get'de bir pozisyon olmamasının tek şartı Pacman'in oyun sahası dışında olmasıdır.
+                Pacman oyununda hatırlanacağı üzere tam ortada ekranın solundan veya sağından
+                çıkılıp ters noktadan tekrar oyuna girilmesi mümkündür.
+
+                Sol tarafan çıkılması halinde pacman'in x değeri -1 dir.
+                Sağ taraftan çıkılma halinde ise bu değer sahanın genişliği ile eş değerdir.
+
+             */
+            None => {
+                if position.x == -1 {
+                    // Pacman sahanın solundan çıktıysa sağ uçtan tekrar giriş yapsın
+                    self.pos.x = TILE_MAP_WIDTH as i32 - 1;
+                } else if position.x == TILE_MAP_WIDTH as i32 {
+                    // pacman sağ uçtan çıktıysa sol uçtan tekrar giriş yapsın
+                    self.pos.x = 0;
+                }
+            }
+            Some(Tile::NotWall(powerup)) => {
+                // Duvar olmadığı sürece Pacman hareketine kesintisiz olarak devam eder.
+                self.pos = position;
+                match powerup {
+                    TileType::Dot => {
+                        // noktanın yenmesi lazım
+                    }
+                    TileType::Empty => {}
+                    TileType::Powerup => {
+                        // powerup kullanımı
+                    }
+                }
+            }
+            _ => (),
+        }
     }
 }
