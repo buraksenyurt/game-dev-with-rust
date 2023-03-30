2e5aec90cf3ab0f90f840edab4444e98fdc05bdf
buraksenyurt <burakselimsenyurt@gmail.com>
2023-03-30 23:41:46 +0300

    Applied from_str trait to command.

--- mystery/src/command.rs
+++ mystery/src/command.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+
 // Oyuncunun terminalden girdiği komutları tanımlayan enum veri yapısı.
 // Sola dön, aynaya bak, merdiveni tırman, şurubu iç gibi ifadeleri temsil etmeye çalışacağız.
 #[derive(Debug, PartialEq)]
@@ -10,23 +12,49 @@ pub enum Command {
     Get(String),
     Give(String),
     Drop(String),
-    Inventory,
+    Inventory(String),
     Quit,
+}
+
+// Command nesnesinin üretimini bir String'den gerçekleştirmek için eklendi
+impl FromStr for Command {
+    type Err = ();
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        let lovercase = input.to_lowercase();
+        let mut parts = lovercase.split_whitespace();
+        let noun = parts.next().unwrap_or_default().to_string();
+        let verb = parts.next().unwrap_or_default().to_string();
+
+        match verb.as_str() {
+            "bak" => Ok(Command::Look(noun)),
+            "git" => Ok(Command::Jump(noun)),
+            "kalk" => Ok(Command::GetUp),
+            "sor" => Ok(Command::Talk(noun)),
+            "al" => Ok(Command::Get(noun)),
+            "ver" => Ok(Command::Give(noun)),
+            "bırak" => Ok(Command::Drop(noun)),
+            "listele" => Ok(Command::Inventory(noun)),
+            "bitir" => Ok(Command::Quit),
+            _ => Ok(Command::Unknown(input.trim().to_string())),
+        }
+    }
 }
 
 #[cfg(test)]
 mod command_tests {
     use crate::command::Command;
     use crate::controller::parse;
+    use std::str::FromStr;
 
     #[test]
     fn should_parse_works() {
         let input = "Etrafa bak";
-        let cmd = parse(input);
+        let cmd = Command::from_str(input).unwrap();
         assert_eq!(cmd, Command::Look("etrafa".to_string()));
 
         let input = "kapıya git";
-        let cmd = parse(input);
+        let cmd = Command::from_str(input).unwrap();
         assert_eq!(cmd, Command::Jump("kapıya".to_string()));
     }
 }
--- mystery/src/controller.rs
+++ mystery/src/controller.rs
@@ -2,6 +2,7 @@ use crate::command::Command;
 use log::info;
 use std::io;
 use std::io::Write;
+use std::str::FromStr;
 
 // Terminalden girdi alıp bunu Command nesnesine çeviren fonksiyon
 pub fn take_input() -> Command {
@@ -13,29 +14,7 @@ pub fn take_input() -> Command {
         .read_line(&mut input)
         .expect("Ne demek istediğini anlayamadım. Üzgünüm.");
     println!();
-    parse(input.as_str())
-}
-
-// girilen metni iki parça olarak ele alır ve Command nesnesine dönüştürüp geri döndürür
-pub fn parse(input: &str) -> Command {
-    let lovercase = input.to_lowercase();
-    let mut parts = lovercase.split_whitespace();
-    let noun = parts.next().unwrap_or_default().to_string();
-    let verb = parts.next().unwrap_or_default().to_string();
-    info!("Parse işlemi. Noun : {} Verb : {}", noun, verb);
-
-    match verb.as_str() {
-        "bak" => Command::Look(noun),
-        "git" => Command::Jump(noun),
-        "kalk" => Command::GetUp,
-        "sor" => Command::Talk(noun),
-        "al" => Command::Get(noun),
-        "ver" => Command::Give(noun),
-        "bırak" => Command::Drop(noun),
-        "aletler" => Command::Inventory,
-        "bitir" => Command::Quit,
-        _ => Command::Unknown(input.trim().to_string()),
-    }
+    Command::from_str(&input).unwrap()
 }
 
 pub fn update_screen(output: String) {
