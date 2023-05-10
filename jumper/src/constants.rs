045343f7e496eaeca494339388c2345e68c8a432
buraksenyurt <burakselimsenyurt@gmail.com>
2023-05-10 17:33:11 +0300

    Rearanged game window.

---
+++ jumper/src/constants.rs
@@ -0,0 +1,2 @@
+pub const SCREEN_WIDTH: f32 = 1000.;
+pub const SCREEN_HEIGHT: f32 = 500.;
--- jumper/src/main.rs
+++ jumper/src/main.rs
@@ -1,3 +1,26 @@
+mod constants;
+
+use crate::constants::{SCREEN_HEIGHT, SCREEN_WIDTH};
+use bevy::app::App;
+use bevy::prelude::*;
+use bevy::window::WindowResolution;
+use bevy::DefaultPlugins;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Jumper - A Series Jumping Game :P".to_string(),
+                resolution: WindowResolution::new(SCREEN_WIDTH, SCREEN_HEIGHT),
+                resizable: false,
+                ..Default::default()
+            }),
+            ..Default::default()
+        }))
+        .add_startup_system(setup_system)
+        .run();
+}
+
+fn setup_system() {
+    println!("2D Platform Game");
 }
