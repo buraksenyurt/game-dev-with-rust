a144c834f25e63b03fd8aa118ba0b2f3accf2587
buraksenyurt <burakselimsenyurt@gmail.com>
2023-05-10 18:13:36 +0300

    Redesigned project structure.

--- jumper/src/main.rs
+++ jumper/src/main.rs
@@ -1,13 +1,13 @@
 mod constants;
-mod pipe;
+mod shapes;
+mod systems;
 
-use crate::constants::{SCREEN_HEIGHT, SCREEN_WIDTH, STD_PIPE_WIDTH};
-use crate::pipe::{spawn_pipe, Pipe};
+use crate::constants::{SCREEN_HEIGHT, SCREEN_WIDTH};
 use bevy::app::App;
 use bevy::prelude::*;
-use bevy::sprite::MaterialMesh2dBundle;
 use bevy::window::WindowResolution;
 use bevy::DefaultPlugins;
+use crate::systems::setup::setup_system;
 
 fn main() {
     App::new()
@@ -23,45 +23,4 @@ fn main() {
         }))
         .add_startup_system(setup_system)
         .run();
-}
-
-fn setup_system(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-) {
-    commands.spawn(MaterialMesh2dBundle {
-        mesh: meshes.add(shape::Circle::default().into()).into(),
-        material: materials.add(ColorMaterial::from(Color::DARK_GREEN)),
-        transform: Transform {
-            translation: Vec3::new(0., 0., 0.),
-            scale: Vec3::new(60., 60., 1.),
-            ..Default::default()
-        },
-        ..default()
-    });
-
-    let gold_pipe = Pipe {
-        x: 0.,
-        y: -SCREEN_HEIGHT * 0.5 + 50.,
-        z: 0.,
-        width: STD_PIPE_WIDTH,
-        hegiht: 100.,
-        z_deep: 1.,
-        color: Color::GOLD,
-    };
-    spawn_pipe(&mut commands, gold_pipe);
-
-    let white_pipe = Pipe {
-        x: SCREEN_WIDTH * 0.1,
-        y: -SCREEN_HEIGHT * 0.5 + 100.,
-        z: 0.,
-        width: STD_PIPE_WIDTH,
-        hegiht: 200.,
-        z_deep: 1.,
-        color: Color::ANTIQUE_WHITE,
-    };
-    spawn_pipe(&mut commands, white_pipe);
-
-    commands.spawn(Camera2dBundle::default());
 }
---
+++ jumper/src/systems/setup.rs
@@ -0,0 +1,45 @@
+use bevy::prelude::*;
+use bevy::sprite::MaterialMesh2dBundle;
+use crate::constants::{SCREEN_HEIGHT, SCREEN_WIDTH, STD_PIPE_WIDTH};
+use crate::shapes::pipe::{Pipe, spawn_pipe};
+
+pub fn setup_system(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: meshes.add(shape::Circle::default().into()).into(),
+        material: materials.add(ColorMaterial::from(Color::DARK_GREEN)),
+        transform: Transform {
+            translation: Vec3::new(0., 0., 0.),
+            scale: Vec3::new(60., 60., 1.),
+            ..Default::default()
+        },
+        ..default()
+    });
+
+    let gold_pipe = Pipe {
+        x: 0.,
+        y: -SCREEN_HEIGHT * 0.5 + 50.,
+        z: 0.,
+        width: STD_PIPE_WIDTH,
+        hegiht: 100.,
+        z_deep: 1.,
+        color: Color::GOLD,
+    };
+    spawn_pipe(&mut commands, gold_pipe);
+
+    let white_pipe = Pipe {
+        x: SCREEN_WIDTH * 0.1,
+        y: -SCREEN_HEIGHT * 0.5 + 100.,
+        z: 0.,
+        width: STD_PIPE_WIDTH,
+        hegiht: 200.,
+        z_deep: 1.,
+        color: Color::ANTIQUE_WHITE,
+    };
+    spawn_pipe(&mut commands, white_pipe);
+
+    commands.spawn(Camera2dBundle::default());
+}
