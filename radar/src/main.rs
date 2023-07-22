7ed78140096340d457c280d50f1862d9683745b6
buraksenyurt <burakselimsenyurt@gmail.com>
2023-07-22 21:05:38 +0300

    Spawned enemy in random radar positions.

--- radar/src/main.rs
+++ radar/src/main.rs
@@ -1,11 +1,19 @@
+//! Radar Game
+//! The aim of this game is to shoot vehicles that appear on the radar.
+//! However, it is necessary to pay attention to the distinction between friend and foe.
+//! Hitting allied forces is minus points.//
+// In general, the basic functionalities of ggez are covered.
+
 use ggez::conf::WindowMode;
 use ggez::graphics::Text;
+use ggez::mint::Point2;
 use ggez::{
     event,
     glam::*,
     graphics::{self, Color},
     Context, GameResult,
 };
+use rand::Rng;
 
 const SCREEN_WIDTH: f32 = 400.;
 const SCREEN_HEIGHT: f32 = 400.;
@@ -24,28 +32,53 @@ pub fn main() -> GameResult {
 struct GameState {
     circle_r: f32,
     score: f32,
+    mouse: Mouse,
+    enemy_x: f32,
+    enemy_y: f32,
+    enemy_is_live: bool,
+    enemy_spawned: bool,
 }
 
+struct Mouse(Point2<f32>);
+
 impl GameState {
-    fn new(_ctx: &mut Context) -> GameResult<GameState> {
+    fn new(ctx: &mut Context) -> GameResult<GameState> {
         Ok(GameState {
             circle_r: 0.,
             score: 0.,
+            mouse: Mouse(ctx.mouse.position()),
+            enemy_x: 0.,
+            enemy_y: 0.,
+            enemy_is_live: false,
+            enemy_spawned: false,
         })
     }
 }
 
 impl event::EventHandler<ggez::GameError> for GameState {
-    fn update(&mut self, _ctx: &mut Context) -> GameResult {
+    fn update(&mut self, ctx: &mut Context) -> GameResult {
+        // Increase radius of radar circle
         if self.circle_r >= SCREEN_WIDTH / 2. {
             self.circle_r = 0.;
         }
-        self.circle_r = self.circle_r + 0.8;
+        self.circle_r = &self.circle_r + 0.8;
+
+        // Calculate random enemey points
+        if ctx.time.ticks() % 120 == 0 {
+            let mut rng = rand::thread_rng();
+            let angle: f32 = rng.gen_range(-360.0..360.);
+            self.enemy_x = SCREEN_WIDTH * 0.5 + &self.circle_r * angle.cos();
+            self.enemy_y = SCREEN_HEIGHT * 0.5 + &self.circle_r * angle.sin();
+            self.enemy_spawned = true;
+        }
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
+
         let mut canvas = graphics::Canvas::from_frame(ctx, Color::BLACK);
+
+        // Radar circle draw
         let circle = graphics::Mesh::new_circle(
             ctx,
             graphics::DrawMode::stroke(2.),
@@ -56,6 +89,7 @@ impl event::EventHandler<ggez::GameError> for GameState {
         )?;
         canvas.draw(&circle, Vec2::new(0., 0.));
 
+        // Grid lines draw
         for i in 1..4 {
             let line = graphics::Mesh::new_line(
                 ctx,
@@ -71,8 +105,8 @@ impl event::EventHandler<ggez::GameError> for GameState {
             let line = graphics::Mesh::new_line(
                 ctx,
                 &[
-                    Vec2::new(i as f32 * 100.,0. ),
-                    Vec2::new(i as f32 * 100.,SCREEN_HEIGHT),
+                    Vec2::new(i as f32 * 100., 0.),
+                    Vec2::new(i as f32 * 100., SCREEN_HEIGHT),
                 ],
                 2.,
                 Color::from_rgb(123, 122, 121),
@@ -80,8 +114,38 @@ impl event::EventHandler<ggez::GameError> for GameState {
             canvas.draw(&line, graphics::DrawParam::from([0., 0.]));
         }
 
+        // Enemey draw
+        if self.enemy_spawned {
+            let enemy_circle = graphics::Mesh::new_circle(
+                ctx,
+                graphics::DrawMode::fill(),
+                vec2(self.enemy_x, self.enemy_y),
+                10.,
+                0.1,
+                Color::WHITE,
+            )?;
+            canvas.draw(&enemy_circle, graphics::DrawParam::from([0., 0.]));
+        }
+
+        // Informal text draw
         let score_text = Text::new(format!("score:{}  r:{}", self.score, self.circle_r));
         canvas.draw(&score_text, graphics::DrawParam::from([0., 0.]));
+
+        // Mouse cursor draw
+        let mouse_line = graphics::Mesh::new_line(
+            ctx,
+            &[Vec2::new(0., -15.), Vec2::new(0., 15.)],
+            2.,
+            Color::from_rgb(165, 82, 76),
+        )?;
+        canvas.draw(&mouse_line, graphics::DrawParam::from(ctx.mouse.position()));
+        let mouse_line = graphics::Mesh::new_line(
+            ctx,
+            &[Vec2::new(-15., 0.), Vec2::new(15., 0.)],
+            2.,
+            Color::from_rgb(165, 82, 76),
+        )?;
+        canvas.draw(&mouse_line, graphics::DrawParam::from(ctx.mouse.position()));
 
         canvas.finish(ctx)?;
 
