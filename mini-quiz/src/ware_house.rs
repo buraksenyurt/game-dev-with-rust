dd657b570a1c690efc3f7b897f30d638915efb12
buraksenyurt <burakselimsenyurt@gmail.com>
2023-08-05 16:44:16 +0300

    Added shuffling to questions.

--- mini-quiz/src/ware_house.rs
+++ mini-quiz/src/ware_house.rs
@@ -1,4 +1,5 @@
 use crate::model::*;
+use rand::prelude::SliceRandom;
 use std::error::Error;
 use std::fs::File;
 use std::io::BufReader;
@@ -10,7 +11,10 @@ impl WareHouse {
     pub fn load_quiz<P: AsRef<Path>>(path: P) -> Result<Vec<Question>, Box<dyn Error>> {
         let file = File::open(path)?;
         let reader = BufReader::new(file);
-        let questions = serde_json::from_reader(reader)?;
+        let mut questions: Vec<Question> = serde_json::from_reader(reader)?;
+
+        let mut rnd = rand::thread_rng();
+        questions.shuffle(&mut rnd);
         Ok(questions)
     }
 }
