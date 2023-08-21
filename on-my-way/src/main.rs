0d9e3b7e1240f512902a209fa3563f8e59848e16
buraksenyurt <burakselimsenyurt@gmail.com>
2023-08-21 18:53:01 +0300

    Added movement system for single meteor.

--- on-my-way/src/main.rs
+++ on-my-way/src/main.rs
@@ -1,23 +1,42 @@
 use bevy::prelude::*;
 use bevy::window::PrimaryWindow;
+use rand::random;
 
 pub const SPACESHIP_001_WIDTH: f32 = 80.;
 pub const SPACESHIP_001_HEIGHT: f32 = 106.;
 pub const SPACESHIP_001_SPEED: f32 = 500.;
+pub const STANDARD_METEOR_SPEED: f32 = 100.;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, (spawn_camera_system, spawn_spaceship_system))
+        .add_systems(
+            Startup,
+            (
+                spawn_camera_system,
+                spawn_spaceship_system,
+                spawn_meteor_system,
+            ),
+        )
         .add_systems(
             Update,
-            (spaceship_movement_system, spaceship_border_check_system),
+            (
+                spaceship_movement_system,
+                spaceship_border_check_system,
+                meteor_movement_system,
+                meteor_outside_of_the_bounds_system
+            ),
         )
         .run();
 }
 
 #[derive(Component)]
 pub struct Spaceship {}
+
+#[derive(Component)]
+pub struct Meteor {
+    pub direction: Vec2,
+}
 
 pub fn spawn_camera_system(
     mut commands: Commands,
@@ -90,5 +109,44 @@ pub fn spaceship_border_check_system(
         }
 
         spaceship_transform.translation = translation;
+    }
+}
+
+pub fn spawn_meteor_system(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+) {
+    let window = window_query.get_single().unwrap();
+    let y = random::<f32>() * window.height();
+    let x = window.width() + random::<f32>() * window.width();
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform::from_xyz(x, y, 0.),
+            texture: asset_server.load("sprites/spaceMeteors_001.png"),
+            ..default()
+        },
+        Meteor {
+            direction: Vec2::new(-1., 0.),
+        },
+    ));
+}
+
+pub fn meteor_movement_system(mut query: Query<(&mut Transform, &Meteor)>, time: Res<Time>) {
+    for (mut transform, meteor) in query.iter_mut() {
+        let direction = Vec3::new(meteor.direction.x, meteor.direction.y, 0.);
+        transform.translation += direction * STANDARD_METEOR_SPEED * time.delta_seconds();
+    }
+}
+
+pub fn meteor_outside_of_the_bounds_system(
+    mut commands: Commands,
+    mut query: Query<(Entity, &Transform), With<Meteor>>,
+) {
+    for (entity, transform) in query.iter_mut() {
+        if transform.translation.x < -50. {
+            commands.entity(entity).despawn();
+            info!("Meteor sınır dışına çıktı.");
+        }
     }
 }
