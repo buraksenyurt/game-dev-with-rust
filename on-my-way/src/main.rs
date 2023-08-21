e1e4939d8816a19bffb6b41fc20ac9644784b4ec
buraksenyurt <burakselimsenyurt@gmail.com>
2023-08-21 19:19:31 +0300

    Applied timer based meteor spawning.

--- on-my-way/src/main.rs
+++ on-my-way/src/main.rs
@@ -1,15 +1,17 @@
 use bevy::prelude::*;
 use bevy::window::PrimaryWindow;
-use rand::random;
+use rand::{random, Rng};
 
 pub const SPACESHIP_001_WIDTH: f32 = 80.;
 pub const SPACESHIP_001_HEIGHT: f32 = 106.;
 pub const SPACESHIP_001_SPEED: f32 = 500.;
-pub const STANDARD_METEOR_SPEED: f32 = 100.;
+pub const STANDARD_METEOR_SPEED: f32 = 150.;
+pub const METEOR_SPAWN_TIME: f32 = 5.;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .init_resource::<MeteorSpawnTimer>()
         .add_systems(
             Startup,
             (
@@ -24,7 +26,9 @@ fn main() {
                 spaceship_movement_system,
                 spaceship_border_check_system,
                 meteor_movement_system,
-                meteor_outside_of_the_bounds_system
+                meteor_outside_of_the_bounds_system,
+                meteor_spawn_tick_counter_system,
+                meteor_spawn_after_timer_system,
             ),
         )
         .run();
@@ -36,6 +40,19 @@ pub struct Spaceship {}
 #[derive(Component)]
 pub struct Meteor {
     pub direction: Vec2,
+}
+
+#[derive(Resource)]
+pub struct MeteorSpawnTimer {
+    pub timer: Timer,
+}
+
+impl Default for MeteorSpawnTimer {
+    fn default() -> Self {
+        Self {
+            timer: Timer::from_seconds(METEOR_SPAWN_TIME, TimerMode::Repeating),
+        }
+    }
 }
 
 pub fn spawn_camera_system(
@@ -112,6 +129,17 @@ pub fn spaceship_border_check_system(
     }
 }
 
+pub fn get_random_meteor() -> String {
+    let meteors = vec![
+        "sprites/spaceMeteors_001.png",
+        "sprites/spaceMeteors_002.png",
+        "sprites/spaceMeteors_003.png",
+        "sprites/spaceMeteors_004.png",
+    ];
+    let idx = rand::thread_rng().gen_range(0..meteors.len());
+    info!("{idx} - {} kullanılacak", meteors[idx]);
+    meteors[idx].to_string()
+}
 pub fn spawn_meteor_system(
     mut commands: Commands,
     window_query: Query<&Window, With<PrimaryWindow>>,
@@ -120,10 +148,14 @@ pub fn spawn_meteor_system(
     let window = window_query.get_single().unwrap();
     let y = random::<f32>() * window.height();
     let x = window.width() + random::<f32>() * window.width();
+    spawn_meteor(&mut commands, asset_server, y, x);
+}
+
+fn spawn_meteor(commands: &mut Commands, asset_server: Res<AssetServer>, y: f32, x: f32) {
     commands.spawn((
         SpriteBundle {
             transform: Transform::from_xyz(x, y, 0.),
-            texture: asset_server.load("sprites/spaceMeteors_001.png"),
+            texture: asset_server.load(get_random_meteor()),
             ..default()
         },
         Meteor {
@@ -148,5 +180,26 @@ pub fn meteor_outside_of_the_bounds_system(
             commands.entity(entity).despawn();
             info!("Meteor sınır dışına çıktı.");
         }
+    }
+}
+
+pub fn meteor_spawn_tick_counter_system(
+    mut meteor_timer: ResMut<MeteorSpawnTimer>,
+    time: Res<Time>,
+) {
+    meteor_timer.timer.tick(time.delta());
+}
+
+pub fn meteor_spawn_after_timer_system(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+    meteor_timer: Res<MeteorSpawnTimer>,
+) {
+    if meteor_timer.timer.finished() {
+        let window = window_query.get_single().unwrap();
+        let y = random::<f32>() * window.height();
+        let x = window.width() + random::<f32>() * window.width();
+        spawn_meteor(&mut commands, asset_server, y, x);
     }
 }
