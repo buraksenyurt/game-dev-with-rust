bff711d1966e50225d56d100e44069e9fe5dbc08
buraksenyurt <burakselimsenyurt@gmail.com>
2024-02-16 22:13:32 +0300

    Added unit test.

--- slam-dunk-manager/tests/integration_tests.rs
+++ slam-dunk-manager/tests/integration_tests.rs
@@ -2,6 +2,7 @@
 mod tests {
     use chrono::Utc;
     use slam_dunk_manager::game_mngr::contest::create_schedule;
+    use slam_dunk_manager::prelude::game::{Game, GameState};
     use slam_dunk_manager::prelude::league::{add_player_team, create_league};
 
     #[test]
@@ -27,5 +28,24 @@ mod tests {
         assert_eq!(league.teams.len(), 8);
         let fixture = create_schedule(&league.teams, Utc::now());
         assert_eq!(fixture.len(), 7);
+    }
+
+    #[tokio::test]
+    async fn test_save_game_and_load_data() {
+        let mut league = create_league();
+        assert_eq!(league.teams.len(), 7);
+        add_player_team("Academy Tokyo", &mut league);
+        assert_eq!(league.teams.len(), 8);
+        let fixture = create_schedule(&league.teams, Utc::now());
+        assert_eq!(fixture.len(), 7);
+        let game = Game {
+            current_state: GameState::Initial,
+            fixture,
+        };
+        assert!(game.save("GamesData.bin").await.is_ok());
+
+        let game = Game::load("GamesData.bin").await;
+        assert!(game.is_ok());
+        assert_eq!(game.unwrap().fixture.len(),7);
     }
 }
