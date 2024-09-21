570096766e7b3e27354f986a79446cbe7cff1803
buraksenyurt <burakselimsenyurt@gmail.com>
2024-09-21 17:28:10 +0300

    Added some instruments.

--- falling_colors/src/main.rs
+++ falling_colors/src/main.rs
@@ -1,5 +1,61 @@
+use std::time::{Duration, Instant};
+const SCREEN_HEIGHT: f32 = 600f32;
+const SCREEN_WIDTH: f32 = 400f32;
+const SQUARE_FALL_SPEED: f32 = 5f32;
+
 fn main() {
     println!("Hello, world!");
+}
+
+struct Game {
+    player: Hero,
+    smart_squares: Vec<SmartSquare>,
+    last_fall: Instant,
+}
+
+impl Game {
+    fn new() -> Self {
+        Self {
+            player: Hero::new(
+                "Antonyo Kunte".to_string(),
+                ShapeSize {
+                    width: 32f32,
+                    height: 32f32,
+                },
+                [0f32, 1f32, 1f32, 1f32],
+            ),
+            smart_squares: vec![],
+            last_fall: Instant::now(),
+        }
+    }
+
+    fn spawn_square(&mut self) {
+        let square = SmartSquare::new(
+            Position {
+                x: rand::random::<f32>() * SCREEN_WIDTH,
+                y: 0f32,
+            },
+            ShapeSize {
+                width: 32f32,
+                height: 32f32,
+            },
+            [rand::random(), rand::random(), rand::random(), 1f32],
+        );
+        self.smart_squares.push(square);
+    }
+
+    fn update(&mut self) {
+        for square in &mut self.smart_squares {
+            square.free_fall(SQUARE_FALL_SPEED);
+        }
+
+        self.smart_squares.retain(|s| !s.is_out_of_screen(SCREEN_HEIGHT));
+
+        if self.last_fall.elapsed() >= Duration::from_secs(1) {
+            self.spawn_square();
+            self.last_fall = Instant::now();
+        }
+    }
 }
 
 struct Position {
@@ -7,7 +63,7 @@ struct Position {
     y: f32,
 }
 
-struct Size {
+struct ShapeSize {
     width: f32,
     height: f32,
 }
@@ -19,12 +75,12 @@ struct Size {
 
 struct SmartSquare {
     pos: Position,
-    size: Size,
+    size: ShapeSize,
     color: [f32; 4],
 }
 
 impl SmartSquare {
-    fn new(pos: Position, size: Size, color: [f32; 4]) -> Self {
+    fn new(pos: Position, size: ShapeSize, color: [f32; 4]) -> Self {
         Self { pos, size, color }
     }
 
@@ -32,7 +88,19 @@ impl SmartSquare {
         self.pos.y += y_speed;
     }
 
-    fn check_y_range(&self, window_height: f32) -> bool {
+    fn is_out_of_screen(&self, window_height: f32) -> bool {
         self.pos.y > window_height
+    }
+}
+
+struct Hero {
+    name: String,
+    size: ShapeSize,
+    color: [f32; 4],
+}
+
+impl Hero {
+    fn new(name: String, size: ShapeSize, color: [f32; 4]) -> Self {
+        Self { name, size, color }
     }
 }
